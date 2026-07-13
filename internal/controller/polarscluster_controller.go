41bd587f400818c1d12f743df27b97f8e58f9a98
Jop Zitman <jop@polars.tech>
2026-07-13 10:36:16 +0200

    Initial commit

---
+++ internal/controller/polarscluster_controller.go
@@ -0,0 +1,47 @@
+package controller
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	computev1 "polars-inc/polars-k8s-operator/api/v1"
+)
+
+// PolarsClusterReconciler reconciles a PolarsCluster object
+type PolarsClusterReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=compute.pola.rs,resources=polarsclusters,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=compute.pola.rs,resources=polarsclusters/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=compute.pola.rs,resources=polarsclusters/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the PolarsCluster object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.24.1/pkg/reconcile
+func (r *PolarsClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = logf.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *PolarsClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&computev1.PolarsCluster{}).
+		Named("polarscluster").
+		Complete(r)
+}
